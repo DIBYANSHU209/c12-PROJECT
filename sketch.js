87508ac3dcde87405650306bc437a49b0c67ee81
DIBYANSHU209 <141745740+DIBYANSHU209@users.noreply.github.com>
2023-09-06 12:25:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,17 +13,17 @@ function preload(){
 
 function setup(){
   
-  createCanvas(400,400);
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
+    createCanvas(400,400);
+  // Moving background//
+  garden=createSprite(200,200);
+  garden.addImage(gardenImg);
 
 
-//creating boy running
-rabbit = createSprite(160,340,20,20);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
-}
+  //creating boy running
+  rabbit = createSprite(160,340,20,20);
+  rabbit.scale =0.09;
+  rabbit.addImage(rabbitImg);
+  }
 
 function draw() {
   background(0);
@@ -35,59 +35,20 @@ function draw() {
   rabbit.collide(edges);
   
    drawSprites();
-  
-
-  
-   if (frameCount % 50 ==0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-      createOrange();
-     }else {
-       createRed();
-     }
-   }
+    var select_sprites = Math.round(random(1,3));
+    if (frameCount % 80 === 0) {
+      if (select_sprites === 1) {
+        createApples();
+      } else if (select_sprites === 2) {
+        createOrange();
+      }else {
+        createRed();
+      }
+    }
 
 
-   if (frameCount % 40 == 0) {
-   if (select_sprites == 1) {
-       createApples();
-    } else if (select_sprites == 2) {
-     createOrange();
-     }
-   }
+  }
 
-   if (frameCount / 80 == 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-     }else {
-      createRed();
-     }
-   }
-
-   if (frameCount % 60 == 0) {
-     if (select_sprites == 1) {
-       createApples();
-     } else if (select_sprites == 2) {
-       createOrange();
-    } else {
-       createRed();
-     }
-   }
-
-
-
-}
-
-var select_sprites = Math.round(random(1,3));
-
- var select_sprites = Math.round(random(1,3));
-
- var select_sprites = Math.round(1,3);
-
- var select_sprites = Math.round(random(1,3));
 
 
 function createApples() {
@@ -111,6 +72,6 @@ function createRed() {
 redL = createSprite(random(50, 350),40, 10, 10);
 redL.addImage(redImg);
 redL.scale=0.06;
-  redL.velocityY = 3;
-  redL.lifetime = 150;
+redL.velocityY = 3;
+redL.lifetime = 150;
 }
